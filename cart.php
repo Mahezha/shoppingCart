9c6e5efa3d3374b1d96502caee0594fdd6c245f5
MaheshaMuthumala <31380743+MaheshaMuthumala@users.noreply.github.com>
2019-04-21 14:56:00 +0530

    Initial commit

---
+++ cart.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+$product_ids = array();
+session_destroy();
+
+if (filter_input(INPUT_POST, 'add_to_cart')){
+    if (isset($_SESSION['shopping_cart'])){
+        $count = count($_SESSION['shopping_cart']);
+        $product_ids = array_column($_SESSION['shopping_cart'],id);
+
+        if (!in_array(filter_input(INPUT_GET, id), $product_ids)){
+            $_SESSION['shopping_cart'][$count] = array(
+                'id' => filter_input(INPUT_GET, 'id'),
+                'name' => filter_input(INPUT_POST, 'name'),
+                'price' => filter_input(INPUT_POST, 'price'),
+                'quantity' => filter_input(INPUT_POST, 'quantity')
+            );
+        }else{
+            for ($i=0; $i<count($product_ids); $i++){
+                if ($product_ids[$i] == filter_input(INPUT_GET, id)){
+                    $_SESSION['shopping_cart'][$i]['quantity'] += filter_input(INPUT_POST, quantity);
+                }
+            }
+        }
+    }else{
+        $_SESSION['shopping_cart'][0] = array(
+            'id' => filter_input(INPUT_GET, 'id'),
+            'name' => filter_input(INPUT_POST, 'name'),
+            'price' => filter_input(INPUT_POST, 'price'),
+            'quantity' => filter_input(INPUT_POST, 'quantity')
+    );
+    }
+}
+//pre_r($_SESSION);
+
+function pre_r($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+?>
+<html>
+    <head>
+        <title>Shopping Cart</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+              integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="cart.css">
+    </head>
+
+    <body>
+    <div class="container">
+        <div class="row">
+        <?php
+        /**
+         * Created by PhpStorm.
+         * User: Maheshi
+         * Date: 4/21/2019
+         * Time: 1:43 AM
+         */
+        $connect = mysqli_connect("localhost", "root", "", "cart");
+        $query = "SELECT * FROM products ORDER by id ASC ";
+
+        $result = mysqli_query($connect, $query);
+
+        if ($result):
+            if (mysqli_num_rows($result)>0):
+                while ($product = mysqli_fetch_assoc($result)):
+                    ?>
+                    <div class="col-md-3">
+                        <form method="post" action="index.php?action=add&id=<?php $product[id]; ?>">
+                            <div class="products">
+                                <img src="<?php echo $product['image']; ?>" class="img-responsive">
+                                <h4 class="text-info"><?php echo $product['name'];?></h4>
+                                <h4>$ <?php echo $product['price'];?></h4>
+                                <input type="text" name="quantity" class="form-control" value="1">
+                                <input type="hidden" name="name" value="<?php echo $product['name'];?>">
+                                <input type="hidden" name="price" value="<?php echo $product['price'];?>">
+                                <input type="submit" name="add_to_cart" class="btn btn-info" value="Add to cart"
+                                style="margin-top: 5px">
+                            </div>
+                        </form>
+                    </div>
+                    <?php
+                endwhile;
+            endif;
+        endif;
+        ?>
+        </div>
+    </div>
+
+    </body>
+</html>
