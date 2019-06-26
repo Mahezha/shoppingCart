d5ab1c26117c7a6a5fd5573bbb6ff7899eaa15c3
MaheshaMuthumala <31380743+MaheshaMuthumala@users.noreply.github.com>
2019-06-27 02:16:26 +0530

    styles

--- cart.php
+++ cart.php
@@ -32,7 +32,12 @@ if (filter_input(INPUT_POST, 'add_to_cart')){
     }
 }
 if (filter_input(INPUT_GET, 'action') == 'delete'){
-    
+    foreach ($_SESSION['shopping_cart'] as $key => $product){
+        if ($product['id'] == filter_input(INPUT_GET, 'id')){
+            unset($_SESSION['shopping_cart'][$key]);
+        }
+    }
+    $_SESSION['shopping_cart'] = array_values($_SESSION['shopping_cart']);
 }
 //pre_r($_SESSION);
 
